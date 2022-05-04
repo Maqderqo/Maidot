e18685013fa915c0f625885748294b4ae963eab8
Ali <aliagayev132@gmail.com>
2022-05-04 20:25:15 +0400

    V1.01

--- main-app.js
+++ main-app.js
@@ -1,15 +1,32 @@
 //#region Hamburger
-{
+function addEvents() {
     const hamburger = document.getElementById("hamburger");
-    function addEvents() {
-        hamburger.addEventListener("click", () => {
-            hamburger.children[0].classList.toggle("rotate-right");
-            hamburger.children[1].classList.toggle("rotate");
-            hamburger.children[2].classList.toggle("rotate-left");
-        });
-    }
+    hamburger.addEventListener("click", () => {
+        hamburger.children[0].classList.toggle("rotate-right");
+        hamburger.children[1].classList.toggle("rotate");
+        hamburger.children[2].classList.toggle("rotate-left");
+        const bgs = document.getElementsByClassName('menubg');
+        console.log(bgs);
+
+        console.log(!bgs[0].classList.contains("openmenu"));
+        if (!bgs[0].classList.contains("openmenu")) {
+            bgs[0].classList.add("openmenu");
+            bgs[0].classList.remove("closemenu");
+            setTimeout(() => {
+                bgs[1].classList.add("openmenu");
+                bgs[1].classList.remove("closemenu");
+            }, 150)
+        } else {
+            bgs[1].classList.remove("openmenu");
+            bgs[1].classList.add("closemenu");
+            setTimeout(() => {
+                bgs[0].classList.remove("openmenu");
+                bgs[0].classList.add("closemenu");
+            }, 250);
+        }
+    });
 }
-//#endregion
+
 
 
 addEvents();
